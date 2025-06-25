c6dfdb99c7cdc47625945106d51585306752b3ad
eshudev123 <s8990405@gmail.com>
2025-06-25 12:36:51 +0530

    Add files via upload

---
+++ javascript test/java.js
@@ -0,0 +1,29 @@
+let n=31;
+let prime=true;
+for (let a=2; a<n; a++);{
+   let (n % a===0);
+    prime=false;
+    break;
+}
+ if(prime && n<1);{
+ console.log( `${n} is  prime`);
+ }
+ else{
+ console.log( ${n} is not prime)
+}
+
+// let b=parseInt((prompt)find number );
+// let c=parseInt((prompt)find number);
+
+// if{
+// (case1);
+//  result=(even number);
+//  break;
+
+//  (case2);
+//  result=(odd number);
+//  break;
+// }
+// console.log(result);
+
+30/10
